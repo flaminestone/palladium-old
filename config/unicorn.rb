05661620b4c4cf7a431f26006fecf5bfe76f6885
flaminestone <flamine@list.ru>
2015-07-13 15:12:18 +0300

    edit unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,7 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "/tmp/unicorn.sock", :backlog => 64
 
 # Logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
@@ -17,5 +17,3 @@ stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
 pid "#{shared_dir}/pids/unicorn.pid"
-
-listen "0.0.0.0:8080", :tcp_nopush => true
