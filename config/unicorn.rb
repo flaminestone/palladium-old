cb80557650288c3447d628d5d9c886545e65c1c4
flaminestone <flamine@list.ru>
2015-07-13 17:55:47 +0300

    add nginx restart to init sh

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,8 +9,8 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets", :backlog => 64
-
+listen "/tmp/unicorn.sock", :backlog => 64
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
 # Logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
 stdout_path "#{shared_dir}/log/unicorn.stdout.log"
--- initializer.sh
+++ initializer.sh
@@ -2,4 +2,5 @@
 service postgresql start
 rake db:migrate
 rake init:configure
-#bundle exec unicorn -c config/unicorn.rb -D
+bundle exec unicorn -c config/unicorn.rb -D
+sudo service nginx restart
