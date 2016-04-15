4b22818161c5ab212e356ecd0985d1d87f2c4e51
Dmitriy Rotatyy <flamine@list.ru>
2016-04-15 16:25:02 +0300

    clear models

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,9 +4,4 @@ class Plan < ActiveRecord::Base
   has_many :runs, dependent: :destroy
   serialize :status
   # after_create :count_plan_status_after_create
-
-  def count_plan_status_after_create
-  self.update(status:[{:name=>Status.find_by_main_status(true).name, :count=>[:id], :data=>[1], :color=>Status.find_by_main_status(true).color}])
-  end
-
 end
