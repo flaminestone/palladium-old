3d87ebbfc0f0b5a712ae7969cf3e99521d2165f5
Dmitriy Rotatyy <kvazilife@gmail.com>
2016-08-15 15:05:02 +0300

    add status bar to products show page - foe all plans. Redesign product show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,18 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
+    # TODO: ATTENTION!! Found n+1 request. Fixme, pls
+    @status_data = {}
+    status_hash = {}
+    Status.pluck(:id, :color).each{|curret_status| status_hash.merge!( {curret_status.first => curret_status.last}) }
+    plans_id = @product.plans.pluck(:id)
+    plans_id.each do |plan_id|
+      data = []
+      ResultSet.where(:plan_id => plan_id).group(:status).count.each do |key, value|
+        data << {'data' => [value], 'color' => status_hash[%r(\d).match(key)[0].to_i] }
+      end
+      @status_data.merge!(plan_id => data)
+      end
   end
 
   # GET /products/new
